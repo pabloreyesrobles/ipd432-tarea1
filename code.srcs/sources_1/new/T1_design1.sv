c7d6055ccadbd84e7dc4995157474087ded6e7ef
pabloreyesrobles <pabloreyes500@gmail.com>
2021-10-22 02:48:39 -0300

    vivado prj added

---
+++ code.srcs/sources_1/new/T1_design1.sv
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.10.2021 02:44:23
+// Design Name: 
+// Module Name: T1_design1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module T1_design1(
+    input clk,
+    input resetN,
+    input PushButton,
+    output IncPulse_out
+    );
+endmodule
